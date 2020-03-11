5b860576ff1326eb4cf9d39059299def5766979f
PruneTruong <truongp@ethz.ch>
2020-03-11 15:34:06 +0100

    refactoring

--- utils_training/utils_CNN.py
+++ utils_training/utils_CNN.py
@@ -58,16 +58,16 @@ def load_checkpoint(model, optimizer, scheduler, filename='checkpoint.pth.tar'):
 def plot_training(image1, image2, kp_map1, kp_map2, computed_reward1, loss, mask_batch1, metrics_per_image,
                   epoch, save_path, name_to_save):
 
-    fig, ((axis1, axis2), (axis3, axis4), (axis5, axis6), (axis7, axis8), (axis9, axis10)) = \
-        plt.subplots(5, 2, figsize=(20, 20))
+    fig, ((axis1, axis2), (axis3, axis4), (axis5, axis6), (axis7, axis8)) = \
+        plt.subplots(4, 2, figsize=(20, 20))
     nbr=1
     size_image = image1[nbr, :, :].shape
     plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.0002, hspace=0.4)
     # plot pair of images, red kp are tp points
     axis1.imshow(image1[nbr, :, :], cmap='gray', vmin=0, vmax=255)
     axis1.scatter(metrics_per_image['{}'.format(nbr)]['to_plot']['tp_kp1'][1],
-                  metrics_per_image['{}'.format(nbr)]['to_plot']['tp_kp1'][0], s=2, color='red')
-    axis1.set_title('epoch{}, img1:original_image'.format(epoch), fontsize='small')
+                  metrics_per_image['{}'.format(nbr)]['to_plot']['tp_kp1'][0], s=4, color='red')
+    axis1.set_title('epoch{}, img1:original_image, \n true positive points in red'.format(epoch), fontsize='small')
 
     im = axis2.imshow(image2[nbr, :, :], cmap='gray', vmin=0, vmax=255)
     axis2.scatter(metrics_per_image['{}'.format(nbr)]['to_plot']['warped_tp_kp1'][1],
@@ -86,55 +86,45 @@ def plot_training(image1, image2, kp_map1, kp_map2, computed_reward1, loss, mask
     fig.colorbar(im4, ax=axis4)
 
     # scatter the points afte NMS and in red the tp points
-    axis5.imshow(np.zeros(size_image), cmap='gray', origin='upper', vmin=0.0, vmax=1.0)
+    axis5.imshow(image1[nbr, :, :], cmap='gray', origin='upper', vmin=0.0, vmax=255.0)
     axis5.scatter(metrics_per_image['{}'.format(nbr)]['to_plot']['keypoints_map1'][1],
                   metrics_per_image['{}'.format(nbr)]['to_plot']['keypoints_map1'][0], s=2,
-                  color='white')
+                  color='green')
     axis5.scatter(metrics_per_image['{}'.format(nbr)]['to_plot']['tp_kp1'][1],
-                  metrics_per_image['{}'.format(nbr)]['to_plot']['tp_kp1'][0], s=2, color='red')
-    axis5.set_title('kp_map after NMS of image1, nbr_kp {}, nbr tp keypoints found {}'.format(
+                  metrics_per_image['{}'.format(nbr)]['to_plot']['tp_kp1'][0], s=4, color='red')
+    axis5.set_title('kp_map after NMS of image1 in green, nbr_kp {}, \n nbr tp keypoints found {} (in red)'.format(
         metrics_per_image['{}'.format(nbr)]['nbr_kp1'],
         metrics_per_image['{}'.format(nbr)]['total_nbr_kp_reward1']), fontsize='medium')
 
-    im6 = axis6.imshow(np.zeros(size_image), cmap='gray', origin='upper', vmin=0.0, vmax=1.0)
+    im6 = axis6.imshow(image2[nbr, :, :], cmap='gray', origin='upper', vmin=0.0, vmax=255.0)
     axis6.scatter(metrics_per_image['{}'.format(nbr)]['to_plot']['keypoints_map2'][1],
                   metrics_per_image['{}'.format(nbr)]['to_plot']['keypoints_map2'][0], s=2,
-                  color='white')
+                  color='green')
     axis6.scatter(metrics_per_image['{}'.format(nbr)]['to_plot']['warped_tp_kp1'][1],
-                  metrics_per_image['{}'.format(nbr)]['to_plot']['warped_tp_kp1'][0], s=2,
+                  metrics_per_image['{}'.format(nbr)]['to_plot']['warped_tp_kp1'][0], s=4,
                   color='red')
-    axis6.set_title('kp_map after NMS of image2, nbr_kp {}'.format(
+    axis6.set_title('kp_map after NMS of image2 in green, nbr_kp {} \n true positive points in red'.format(
         metrics_per_image['{}'.format(nbr)]['nbr_kp2']), fontsize='small')
     fig.colorbar(im6, ax=axis6)
 
     # plot the reward and the loss function
     axis7.imshow(computed_reward1[nbr, :, :], vmin=0, vmax=1, interpolation='nearest')
-    axis7.set_title('computed reward:,\nrepeatability={}, total_nbr_tp_kp={}'.format(
+    axis7.set_title('computed reward:,\nrepeatability={}, total_nbr_tp_kp={} \n binary mask for backpropagation sum={}'.format(
         metrics_per_image['{}'.format(nbr)]['repeatability'],
-        metrics_per_image['{}'.format(nbr)]['total_nbr_kp_reward1']), fontsize='small')
+        metrics_per_image['{}'.format(nbr)]['total_nbr_kp_reward1'],
+    np.sum(mask_batch1[nbr, :, :])), fontsize='small')
 
-    '''
-    im8 = axis8.imshow(loss[nbr, :, :], cmap='gray', interpolation='nearest')
-    axis8.set_title('summed loss {},  max {},\n min {}, mean_part={}'. \
-                    format(round(np.sum(loss[nbr]), 2), round(np.max(loss[nbr]), 5),
-                           round(np.min(loss[nbr]), 2), round(np.mean(loss[nbr]), 5)),
-                    fontsize='medium')
-    fig.colorbar(im8, ax=axis8)
-    '''
 
-    # plot the mask and the transformed image according to estimated homography
-    axis9.imshow(mask_batch1[nbr, :, :], vmin=0, vmax=1)
-    axis9.set_title('mask for backpropagation, sum {}'.format(np.sum(mask_batch1[nbr, :, :])))
-
-    im10 = axis10.imshow(
+    # plot the transformed image according to estimated homography
+    im8 = axis8.imshow(
         cv2.warpPerspective(image1[nbr, :, :], metrics_per_image['{}'.format(nbr)]
         ['computed_H'], (size_image[1], size_image[0])), cmap='gray', vmin=0, vmax=255)
-    axis10.set_title('warped image 1 according to computed homography,\n inlier_ratio={}, '
+    axis8.set_title('warped image 1 according to computed homography,\n inlier_ratio={}, '
                      'true_homo={}, class_acceptable={}'.format(
         metrics_per_image['{}'.format(nbr)]['inlier_ratio'],
         metrics_per_image['{}'.format(nbr)]['homography_correct'],
         metrics_per_image['{}'.format(nbr)]['class_acceptable']), fontsize='small')
-    fig.colorbar(im10, ax=axis10)
+    fig.colorbar(im8, ax=axis8)
     fig.savefig(os.path.join(save_path, name_to_save + '.jpg'), bbox_inches='tight')
     plt.close(fig)
 
